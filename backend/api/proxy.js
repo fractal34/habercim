f50846f22d966a29961db094d66e6bc49e8d307a
fractal <fractalintelligent@gmail.com>
2025-04-19 17:25:48 +0200

    Add CORS headers to proxy.js

--- backend/api/proxy.js
+++ backend/api/proxy.js
@@ -1,22 +1,23 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const url = req.query.url;
-    if (!url) {
-        res.status(400).send('URL parameter is required');
-        return;
-    }
+  const url = req.query.url;
+  if (!url) {
+    res.status(400).send('URL parameter is required');
+    return;
+  }
 
-    try {
-        const response = await fetch(url, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            },
-        });
-        const text = await response.text();
-        res.setHeader('Content-Type', 'text/xml');
-        res.send(text);
-    } catch (error) {
-        res.status(500).send(`Error fetching the URL: ${error.message}`);
-    }
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+
+    // CORS başlıklarını ekle
+    res.setHeader('Access-Control-Allow-Origin', 'https://habercim.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).send(`Error fetching URL: ${error.message}`);
+  }
 };
