9d8ab77d02c69f014b04f83a8871259108226517
fractal <fractalintelligent@gmail.com>
2025-04-19 17:33:22 +0200

    Add CORS headers to proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,8 @@
-const fetch = require('node-fetch');
+// /api/proxy.js
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const url = req.query.url;
+
   if (!url) {
     res.status(400).send('URL parameter is required');
     return;
@@ -9,15 +10,14 @@ module.exports = async (req, res) => {
 
   try {
     const response = await fetch(url);
-    const text = await response.text();
+    const data = await response.text();
 
-    // CORS başlıklarını ekle
-    res.setHeader('Access-Control-Allow-Origin', 'https://habercim.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // ya da sadece 'https://habercim.vercel.app'
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    res.status(200).send(text);
+    res.status(200).send(data);
   } catch (error) {
     res.status(500).send(`Error fetching URL: ${error.message}`);
   }
-};
+}
